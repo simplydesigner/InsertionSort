b4f19bb716a548976b22cdbe1bbe0eba0f5abdf8
simplydesigner <simplydesigner@ya.ru>
2016-07-26 16:30:57 +0300

    Initial Commit

---
+++ InsertionSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InsertionSort
+//
+//  Created by simplydesigner on 7/26/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
