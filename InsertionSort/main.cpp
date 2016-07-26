662e9c108379b7a9cd32cbab29152cf2eed2961f
simplydesigner <simplydesigner@ya.ru>
2016-07-26 16:43:23 +0300

    Initialized

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -1,15 +1,29 @@
-//
-//  main.cpp
-//  InsertionSort
-//
-//  Created by simplydesigner on 7/26/16.
-//  Copyright © 2016 simplydesigner. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+auto insertionSort( std::vector<int> & vector )
+{
+    auto size = vector.size();
+    for ( int i = 1; i < size; ++i ) {
+        auto element = vector[ i ];
+        
+        auto j = i;
+        while ( j != 0 && vector[j - 1] > element ) {
+            vector[j] = vector[j - 1];
+            j = j - 1;
+        }
+        
+        vector[j] = element;
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::vector<int> v = { 2, 3, 4, 5, 5, 9, 1 };
+    insertionSort(v);
+    std::ostream_iterator<int> out_it (std::cout,", ");
+    std::copy(v.begin(), v.end(), out_it);
+    
     return 0;
 }
